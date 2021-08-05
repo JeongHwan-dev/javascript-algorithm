660014d98e3a67b6ff008d08c1f4953a7524fee5
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-08-05 23:30:07 +0900

    08.05. study

--- 01-basic/sol5.js
+++ 01-basic/sol5.js
@@ -10,6 +10,20 @@ function solution(arr) {
   return min;
 }
 
+function solution2(arr) {
+  let answer = Math.min(...arr);
+
+  return answer;
+}
+
+function solution3(arr) {
+  let answer = Math.min.apply(null, arr);
+
+  return answer;
+}
+
 let arr = [5, 7, 1, 3, 2, 9, 11];
 
 console.log(solution(arr));
+console.log(solution2(arr));
+console.log(solution3(arr));
