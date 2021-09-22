723b456b2f1c8638c695ab7a821f4732353c4925
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-09-22 15:52:56 +0900

    Review : 세 수 중 최소값

--- 01-basic/sol01.js
+++ 01-basic/sol01.js
@@ -1,11 +1,30 @@
-function solution(a, b, c) {
-  let answer;
+// Solution 1
+// if, else 문을 이용해서 최소값 도출
+function solution1(a, b, c) {
+  let minNum;
 
-  if (a < b) answer = a;
-  else answer = b;
-  if (c < answer) answer = c;
+  if (a < b) {
+    minNum = a;
+  } else {
+    minNum = b;
+  }
 
-  return answer;
+  if (c < minNum) {
+    minNum = c;
+  }
+
+  return minNum;
 }
 
-console.log(solution(2, 5, 1));
+// Solution 2
+// Math.min() 메소드 이용해서 최소값 도출
+function solution2(a, b, c) {
+  return Math.min(a, b, c);
+}
+
+let a = 6;
+let b = 5;
+let c = 11;
+
+console.log(solution1(a, b, c));
+console.log(solution2(a, b, c));
