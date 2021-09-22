b34b669a05879224cc6a3bf3706f8b418347f6a8
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-09-22 21:00:08 +0900

    Review : 1부터 N까지의 합

--- 01-basic/sol04.js
+++ 01-basic/sol04.js
@@ -1,10 +1,19 @@
+// Solution 1
+// 1. for문을 사용해 1 ~ n 까지 계속 더히기
 function solution(n) {
-  let answer = 0;
+  let sum = 0;
 
   for (let i = 1; i <= n; i++) {
-    answer += i;
+    sum += i;
   }
-  return answer;
+
+  return sum;
 }
 
-console.log(solution(10));
+let n = 6;
+
+console.log(solution(n));
+
+n = 10;
+
+console.log(solution(n));
