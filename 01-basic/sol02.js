f0b24614a98d09f0497f2df2f64a25ad7b179e4a
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-09-23 13:47:28 +0900

    Add : '삼각현 판별하기' 솔루션 추가

--- 01-basic/sol02.js
+++ 01-basic/sol02.js
@@ -1,6 +1,7 @@
 // Solution 1
 // 1. if, else 문을 사용해 세 개의 막대 길이 중 가장 긴 길이를 구하고
-// 2. 삼각형 가능 여부 판별 (삼각형의 가장 긴 변의 길이는 나머지 두 변의 길이보다 크거나 같을 수 없다.)
+// 2. if 문을 사용해 삼각형 가능 여부 판별
+// (삼각형의 가장 긴 변의 길이는 나머지 두 변의 길이보다 크거나 같을 수 없다.)
 function solution1(a, b, c) {
   let answer = 'YES';
   let max;
@@ -25,7 +26,8 @@ function solution1(a, b, c) {
 
 // Solution 2
 // 1. Math.max() 메서드를 사용해 세 값 중 최대값 구하고
-// 2. 삼각형 가능 여부 판별 (삼각형의 가장 긴 변의 길이는 나머지 두 변의 길이보다 크거나 같을 수 없다.)
+// 2. if 문을 사용해 삼각형 가능 여부 판별
+// (삼각형의 가장 긴 변의 길이는 나머지 두 변의 길이보다 크거나 같을 수 없다.)
 function solution2(a, b, c) {
   const sum = a + b + c;
   const max = Math.max(a, b, c);
@@ -37,12 +39,24 @@ function solution2(a, b, c) {
   }
 }
 
+// Solution 2
+// 1. Math.max() 메서드를 사용해 세 값 중 최대값 구하고
+// 2. 삼항 연산자를 사용해 삼각형 가능 여부 판별
+// (삼각형의 가장 긴 변의 길이는 나머지 두 변의 길이보다 크거나 같을 수 없다.)
+function solution3(a, b, c) {
+  const sum = a + b + c;
+  const max = Math.max(a, b, c);
+
+  return sum - max > max ? 'YES' : 'NO';
+}
+
 let a = 6;
 let b = 7;
 let c = 11;
 
 console.log(solution1(a, b, c));
 console.log(solution2(a, b, c));
+console.log(solution3(a, b, c));
 
 a = 13;
 b = 33;
@@ -50,3 +64,4 @@ c = 17;
 
 console.log(solution1(a, b, c));
 console.log(solution2(a, b, c));
+console.log(solution3(a, b, c));
