9f56041a84d3cf1c5443e25ec087dd8d09736cf4
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-09-24 09:33:37 +0900

    Review : 대소문자 변환

--- 01-basic/sol13.js
+++ 01-basic/sol13.js
@@ -1,4 +1,11 @@
-function solution(str) {
+// Solution 1
+// 1. 변환된 str을 담을 convertedStr 변수를 빈 문자열로 초기화
+// 2. for 문을 사용해 문자열을 문자 단위로 탐색
+// 3. 탐색하는 문자가 소문자로 변환했을 때와 동일하다면 그 문자는 소문자일 것이다. 그러므로 대문자로 변환 후
+// 4. 변환된 문자를 convertedStr 변수에 더하기
+// 5. 이 조건이 아닌 문자는 대문자이므로 소문자로 변환 후
+// 5. 변환된 문자를 convertedStr 변수에 더하기
+function solution1(str) {
   let convertedStr = '';
 
   for (let c of str) {
@@ -12,6 +19,45 @@ function solution(str) {
   return convertedStr;
 }
 
+// Solution 2
+// 1. 변환된 str을 담을 convertedStr 변수를 빈 문자열로 초기화
+// 2. for 문을 사용해 문자열을 문자 단위로 탐색
+// 3. charCodeAt() 메서드를 사용해 문자의 유니코드 반환
+// 4. 아스키코드를 통해 문자 대소문자 판단
+// (영문 대문자 아스키코드 65 ~ 90 / 영문 소문자 아스키코드 97 ~ 122)
+// 5. 소문자이면 대문자로 변경 후 convertedStr 변수에 더하고
+// 6. 대문자이면 소문자로 변경 후 convertedStr 변수에 더하기
+function solution2(str) {
+  let convertedStr = '';
+
+  for (c of str) {
+    const charCode = c.charCodeAt();
+
+    if (charCode >= 97 && charCode <= 122) {
+      convertedStr += c.toUpperCase();
+    } else {
+      convertedStr += c.toLowerCase();
+    }
+  }
+
+  return convertedStr;
+}
+
+// Solution 3
+// 1. split() 메서드를 사용하여 문자 단위로 쪼개어 새 배열을 생성
+// 2. 생성된 배열의 요소 마다 확인하여 소문자이면 대문자로 변환, 대문자이면 소문자로 변환해서 새 배열 생성
+// 3. join() 메서드를 사용해 변환된 문자열 생성
+function solution3(str) {
+  const convertedStr = str
+    .split('')
+    .map((c) => (c === c.toLowerCase() ? c.toUpperCase() : c.toLowerCase()))
+    .join('');
+
+  return convertedStr;
+}
+
 let str = 'StuDY';
 
-console.log(solution(str));
+console.log(solution1(str));
+console.log(solution2(str));
+console.log(solution3(str));
--- README.md
+++ README.md
@@ -45,7 +45,7 @@
 |  10  |    문자 찾기     | [JS](https://github.com/JeongHwan-dev/javascript-algorithm/blob/master/01-basic/sol10.js) |  08.11.  |  09.23.  |
 |  11  |   대문자 찾기    | [JS](https://github.com/JeongHwan-dev/javascript-algorithm/blob/master/01-basic/sol11.js) |  08.12.  |  09.23.  |
 |  12  |   대문자 통일    | [JS](https://github.com/JeongHwan-dev/javascript-algorithm/blob/master/01-basic/sol12.js) |  08.13.  |  09.23.  |
-|  13  |  대소문자 변환   | [JS](https://github.com/JeongHwan-dev/javascript-algorithm/blob/master/01-basic/sol13.js) |  08.16.  |    -     |
+|  13  |  대소문자 변환   | [JS](https://github.com/JeongHwan-dev/javascript-algorithm/blob/master/01-basic/sol13.js) |  08.16.  |  09.24.  |
 |  14  |  가장 긴 문자열  | [JS](https://github.com/JeongHwan-dev/javascript-algorithm/blob/master/01-basic/sol14.js) |  08.16.  |    -     |
 |  15  | 가운데 문자 출력 | [JS](https://github.com/JeongHwan-dev/javascript-algorithm/blob/master/01-basic/sol15.js) |  08.17.  |    -     |
 |  16  |   중복문자제거   | [JS](https://github.com/JeongHwan-dev/javascript-algorithm/blob/master/01-basic/sol16.js) |  08.18.  |    -     |
