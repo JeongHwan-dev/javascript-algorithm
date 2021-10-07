3ec60841bae648cf75f5ac52572675746893e6a9
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-10-07 19:39:12 +0900

    Review : 연속부분수열1

--- 05-efficiency/sol36.js
+++ 05-efficiency/sol36.js
@@ -1,25 +1,33 @@
-function solution(m, arr) {
-  let answer = 0;
-  let lt = 0;
-  let sum = 0;
+// Solution 1
+// 1. m 값이 만들어지는 연속부분수열 개수를 카운트할 count 변수를 0으로 초기화하여 생성
+// 2. 연속부분수열의 합을 저장할 sum 함수 0으로 초기화
+// 3. 투 포인터 중 start 값을 0으로 초기화
+// 4. for 문을 사용한 투포인터로 sum 값이 m 값과 동일할 때 count 를 +1 증가시키고
+// 5. while 문을 사용해 sum 값이 m값 보다 작을 때까지 start 포인터가 가리키는 값을 빼주고
+// 6. sum이 m값과 같은지 확인 후 같다면 count 를 +1 증가
 
-  for (let rt = 0; rt < arr.length; rt++) {
-    sum += arr[rt];
+function solution(m, arr) {
+  let count = 0;
+  let sum = 0;
+  let start = 0;
+
+  for (let end = 0; end < arr.length; end++) {
+    sum += arr[end];
 
     if (sum === m) {
-      answer++;
+      count++;
     }
 
     while (sum >= m) {
-      sum -= arr[lt++];
+      sum -= arr[start++];
 
       if (sum === m) {
-        answer++;
+        count++;
       }
     }
   }
 
-  return answer;
+  return count;
 }
 
 let m = 6;
