92afa3d18f50afff637c91855f97ac73c38d3d91
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-10-27 11:23:08 +0900

    Review : 버블 정렬

--- 07-selectionSort/sol50.js
+++ 07-selectionSort/sol50.js
@@ -1,20 +1,36 @@
-function solution(arr) {
-  let answer = arr; // 얕은 복사
+// Solution 1
+// 버블 정렬을 이용한 솔루션(뒤부터 정렬)
+function solution1(arr) {
+  const _arr = arr.slice();
 
-  // 버블정렬
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      // 앞에 값이 더 작으면
-      if (arr[j] > arr[j + 1]) {
-        // 교체
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+  for (let i = 0; i < _arr.length - 1; i++) {
+    for (let j = 0; j < _arr.length - i - 1; j++) {
+      if (_arr[j] > _arr[j + 1]) {
+        [_arr[j], _arr[j + 1]] = [_arr[j + 1], _arr[j]];
       }
     }
   }
 
-  return answer;
+  return _arr;
+}
+
+// Solution 2
+// 버블 정렬을 이용한 솔루션(앞부터 정렬)
+function solution2(arr) {
+  const _arr = arr.slice();
+
+  for (let i = 0; i < _arr.length - 1; i++) {
+    for (let j = _arr.length - 1; j > i; j--) {
+      if (_arr[j] < _arr[j - 1]) {
+        [_arr[j], _arr[j - 1]] = [_arr[j - 1], _arr[j]];
+      }
+    }
+  }
+
+  return _arr;
 }
 
 let arr = [13, 5, 11, 7, 23, 15];
 
-console.log(solution(arr));
+console.log(solution1(arr));
+console.log(solution2(arr));
