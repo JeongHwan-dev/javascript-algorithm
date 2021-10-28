b2c24db5716dd5f4c38167389533044fd2aecdf3
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-10-28 16:44:40 +0900

    Review : 삽입 정렬

--- 07-selectionSort/sol04.js
+++ 07-selectionSort/sol04.js
@@ -1,31 +1,24 @@
-// 삽입정렬 사용
+// Solution 1
+// 삽입 정렬을 활용한 솔루션
 function solution(arr) {
-  // 배열 얕은 복사
-  let answer = arr;
+  const _arr = arr.slice();
 
-  for (let i = 0; i < arr.length; i++) {
-    let tmp = arr[i]; // 현재 값
+  for (let i = 1; i < _arr.length; i++) {
+    const cur = _arr[i];
     let j;
 
-    // 현재의 값보다 앞 인덱스에 위치 한 값들 탐색
     for (j = i - 1; j >= 0; j--) {
-      // 만약 현재 값보다 더 큰 값이 있다면
-      if (arr[j] > tmp) {
-        // 위치 변경
-        arr[j + 1] = arr[j];
-      }
-      // 더 작다면
-      else {
-        // 반복문 종료
+      if (_arr[j] > cur) {
+        _arr[j + 1] = _arr[j];
+      } else {
         break;
       }
     }
 
-    // 반복문 종료된 위치에서 +1 인덱스 자리에 현재 값 넣기
-    arr[j + 1] = tmp;
+    _arr[j + 1] = cur;
   }
 
-  return answer;
+  return _arr;
 }
 
 let arr = [11, 7, 5, 6, 10, 9];
