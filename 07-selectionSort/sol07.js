583615520b376a107b6f6e2159c824ee7c1ec517
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-10-31 22:56:55 +0900

    Add : '좌표 정렬' 솔루션 코드 추가

--- 07-selectionSort/sol07.js
+++ 07-selectionSort/sol07.js
@@ -9,6 +9,17 @@ function solution(arr) {
   return sortedArr;
 }
 
+// Solution 2
+// sort() 메서드와 구조 분해 할당을 이용한 솔루션
+function solution2(arr) {
+  const _arr = arr.map((coordinate) => coordinate.slice());
+  const sortedArr = _arr.sort(([x1, y1], [x2, y2]) =>
+    x1 === x2 ? y1 - y2 : x1 - x2
+  );
+
+  return sortedArr;
+}
+
 let arr = [
   [2, 7],
   [1, 3],
