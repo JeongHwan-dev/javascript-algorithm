153d7b18b053f1756c47804a739f79bdaf0990df
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-12-14 21:34:12 +0900

    Review : 조합수

--- 08-recursive&DFS/sol12.js
+++ 08-recursive&DFS/sol12.js
@@ -1,9 +1,7 @@
-function solution(n, r) {
-  let answer;
-  // 3행 5열 배열 생성
-  let dy = Array.from(Array(35), () => Array(35).fill(0));
+// Solution 1
+function solution1(n, r) {
+  const dy = Array.from(Array(35), () => Array(35).fill(0));
 
-  // 깊이 우선 탐색 이용
   function DFS(n, r) {
     if (dy[n][r] > 0) {
       return dy[n][r];
@@ -16,17 +14,17 @@ function solution(n, r) {
     }
   }
 
-  answer = DFS(n, r);
+  const result = DFS(n, r);
 
-  return answer;
+  return result;
 }
 
 let n = 5;
 let r = 3;
 
-console.log(solution(n, r));
+console.log(solution1(n, r));
 
 n = 33;
 r = 19;
 
-console.log(solution(n, r));
+console.log(solution1(n, r));
