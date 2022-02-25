2164ffa8c47b37abe1bde30487b488dd8af5d3f5
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-02-25 16:09:33 +0900

    Solve : 부분집합 구하기

--- 08-recursive&DFS/sol04.js
+++ 08-recursive&DFS/sol04.js
@@ -3,7 +3,7 @@ function solution1(n) {
   const subsetArray = [];
   const check = Array.from({ length: n + 1 }, () => 0);
 
-  function DFS(v) {
+  function dfs(v) {
     if (v === n + 1) {
       let temp = '';
 
@@ -18,13 +18,44 @@ function solution1(n) {
       }
     } else {
       check[v] = 1;
-      DFS(v + 1);
+      dfs(v + 1);
       check[v] = 0;
-      DFS(v + 1);
+      dfs(v + 1);
     }
   }
 
-  DFS(1);
+  dfs(1);
+
+  return subsetArray;
+}
+
+// Solution 2
+function solution2(n) {
+  const subsetArray = [];
+  const check = Array.from({ length: n + 1 }, () => 0);
+
+  function dfs(l) {
+    if (l === n + 1) {
+      const subset = [];
+
+      check.forEach((value, index) => {
+        if (value === 1) {
+          subset.push(index);
+        }
+      });
+
+      if (subset.length > 0) {
+        subsetArray.push(subset);
+      }
+    } else {
+      check[l] = 1;
+      dfs(l + 1);
+      check[l] = 0;
+      dfs(l + 1);
+    }
+  }
+
+  dfs(1);
 
   return subsetArray;
 }
@@ -32,3 +63,4 @@ function solution1(n) {
 let n = 3;
 
 console.log(solution1(n));
+console.log(solution2(n));
