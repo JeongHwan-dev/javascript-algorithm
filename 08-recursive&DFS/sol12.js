57bae1dd463ddd06efbd98adce32a9974ff91203
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-03-03 00:36:54 +0900

    Solve : 조합의 경우수

--- 08-recursive&DFS/sol12.js
+++ 08-recursive&DFS/sol12.js
@@ -2,7 +2,7 @@
 function solution1(n, r) {
   const dy = Array.from(Array(35), () => Array(35).fill(0));
 
-  function DFS(n, r) {
+  function dfs(n, r) {
     if (dy[n][r] > 0) {
       return dy[n][r];
     }
@@ -10,21 +10,41 @@ function solution1(n, r) {
     if (n === r || r === 0) {
       return 1;
     } else {
-      return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+      return (dy[n][r] = dfs(n - 1, r - 1) + dfs(n - 1, r));
     }
   }
 
-  const result = DFS(n, r);
+  const result = dfs(n, r);
 
   return result;
+}
+
+// Solution 2
+function getNFactorial(num) {
+  function dfs(num) {
+    return num === 1 ? 1 : num * dfs(num - 1);
+  }
+
+  const nFactorial = dfs(num);
+
+  return nFactorial;
+}
+
+function solution2(n, r) {
+  const combinationCount =
+    getNFactorial(n) / (getNFactorial(n - r) * getNFactorial(r));
+
+  return combinationCount;
 }
 
 let n = 5;
 let r = 3;
 
 console.log(solution1(n, r));
+console.log(solution2(n, r));
 
 n = 33;
 r = 19;
 
 console.log(solution1(n, r));
+console.log(solution2(n, r));
