892d5dbb6b9e1647838966696ed93595e870d4bb
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-03-17 01:38:19 +0900

    Solve : 돌다리 건너기

--- 10-DynamicProgramming/sol03.js
+++ 10-DynamicProgramming/sol03.js
@@ -1,24 +1,24 @@
+// Solution 1
 function solution(n, arr) {
-  let answer = 0;
-  const dy = Array.from({ length: n }, () => 0);
+  const dp = Array.from({ length: n }, () => 0);
 
-  dy[0] = 1;
+  dp[0] = 1;
 
   for (let i = 1; i < n; i++) {
     let max = 0;
 
     for (let j = i - 1; j >= 0; j--) {
-      if (arr[j] < arr[i] && dy[j] > max) {
-        max = dy[j];
+      if (arr[j] < arr[i] && dp[j] > max) {
+        max = dp[j];
       }
     }
 
-    dy[i] = max + 1;
+    dp[i] = max + 1;
   }
 
-  answer = Math.max(...dy);
+  const maxLength = Math.max(...dp);
 
-  return answer;
+  return maxLength;
 }
 
 let n = 8;
